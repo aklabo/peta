49c5ed0ac1480ad779fca5c9a1459076896a5e48
mass10 <mass10.github@gmail.com>
2016-12-20 00:37:48 +0900

    wip

--- peta-core/resources/views/dashboard.blade.php
+++ peta-core/resources/views/dashboard.blade.php
@@ -9,19 +9,21 @@
 		<title>peta</title>
 		<style>
 		html {
-			width: 100%;
-			height: 100%;
+			width: 100%; height: 100%;
 		}
-
 		body {
-			width: 100%;
-			height: 100%;
+			width: 100%; height: 100%;
+		}
+		.div-cambus {
+			height: 84%; width: 90%; border-style: solid; border-color: #999999;
 		}
 		</style>
+		<script src="https://unpkg.com/react@15/dist/react.min.js"></script>
+		<script src="https://unpkg.com/react-dom@15/dist/react-dom.min.js"></script>
 	</head>
 	<body>
 		<h2>dashboard</h2>
-		<hr>
+		<div class="div-cambus">
+		</div>
 	</body>
 </html>
-
