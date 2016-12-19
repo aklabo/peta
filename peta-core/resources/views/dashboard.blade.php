4da5856943a9fed3d341f61826d4cf56ef386e5f
mass10 <mass10.github@gmail.com>
2016-12-20 00:27:26 +0900

    wip

---
+++ peta-core/resources/views/dashboard.blade.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>peta</title>
+		<style>
+		html {
+			width: 100%;
+			height: 100%;
+		}
+
+		body {
+			width: 100%;
+			height: 100%;
+		}
+		</style>
+	</head>
+	<body>
+		<h2>dashboard</h2>
+		<hr>
+	</body>
+</html>
+
