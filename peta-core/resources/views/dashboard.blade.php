d1d7b025f86702b5da1e0cd793c28268bf931f85
mass10 <mass10.github@gmail.com>
2016-12-24 12:25:28 +0900

    なんか貼れるようになった／Bootstrap で整えた

--- peta-core/resources/views/dashboard.blade.php
+++ peta-core/resources/views/dashboard.blade.php
@@ -6,7 +6,9 @@
 <!DOCTYPE html>
 <html>
 	<head>
+
 		<title>peta</title>
+
 		<style>
 		html {
 			width: 100%;
@@ -16,11 +18,12 @@
 		body {
 			width: 100%;
 			height: 100%;
+			padding-top: 70px;
 		}
 
 		.div-canvas {
-			height: 400px;
-			width: 700px;
+			height: 500px;
+			width: 100%;
 			border-style: solid;
 			border-width: 1px;
 			border-color: #999999;
@@ -40,14 +43,32 @@
 		.div-input-box {
 			background-color: #ffffff;
 			position: absolute;
-			visibility: none;
+			visibility: hidden;
 			width: 300px;
 			height: 200px;
+			left: 0px;
+			top: 0px;
+		}
+
+		.btn-default-xxxxxxxxxx {
+			/*height: 24px;*/
+			font-size: 14px;
 		}
 		</style>
+
+		<script src="/js/jquery-3.1.1.min.js"></script>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<!-- React 諦め中... -->
 		<!-- script src="https://unpkg.com/react@15/dist/react.min.js"></script -->
 		<!-- script src="https://unpkg.com/react-dom@15/dist/react-dom.min.js"></script -->
-		<script src="/js/jquery-3.1.1.min.js"></script>
+
 		<script>
 		function onClick(event) {
 
@@ -87,6 +108,7 @@
 				x: window.event.x - canvas_position.x,
 				y: window.event.y - canvas_position.y
 			};
+
 			dragging_position = {
 				x: window.event.x,
 				y: window.event.y
@@ -95,59 +117,170 @@
 
 		function _show_inputbox() {
 
+			$("#myModal").modal("show");
+		}
 
+		function _show_inputbox_x() {
+
+			var inputbox = document.getElementById("div-input-box");
+			if (inputbox == null)
+				return;
+			var left = ($(window).width() - $("#div-input-box").width()) / 2.0;
+			var top = ($(window).height() - $("#div-input-box").height()) / 2.0;
+			inputbox.style.left = "" + left + "px";
+			inputbox.style.top = "" + top + "px";
+			inputbox.style.visibility = "visible";
+			return true;
 		}
 
 		function _on_dragend_canvas() {
 
 			if(dragging_position == null)
 				return;
-
-			//
-			// event position
-			//
 			var current_position = {x: window.event.x, y: window.event.y};
+			if (!_show_inputbox())
+				return;
+		}
 
-			if (!_show_inputbox()) {
-				//
-				// canceled...
-				//
+		function _onclick_text_cancel() {
+
+			_hide_inputbox();
+		}
+
+		function _hide_inputbox() {
+
+			$("#myModal").modal("hide");
+		}
+
+		function _hide_inputbox_xxx() {
+
+			var inputbox = document.getElementById("div-input-box");
+			if (inputbox == null)
+				return;
+			inputbox.style.visibility = "hidden";
+		}
+
+		function _pop_comment_value() {
+
+			var comment = $("#comment").val();
+			$("#comment").val("");
+			return comment;
+		}
+
+		function _on_click_apply() {
+
+			_hide_inputbox();
+
+			if (dragging_position == null) {
+				alert("ASSERTION FAILURE");
 				return;
 			}
 
-			//
-			// creating a new box.
-			//
+			var comment = _pop_comment_value();
 			var new_element = document.createElement('DIV'); 
 			new_element.id = "";
-			new_element.innerHTML = "<marquee>新しい要素</marquee>";
+			new_element.innerHTML = comment;
 			new_element.className = "div-box";
 			new_element.style.left = "" + dragging_position.x + "px";
 			new_element.style.top = "" + dragging_position.y + "px";
-
-			//
-			// appending to canvas.
-			//
 			var canvas = document.getElementById("div-canvas");
 			if (canvas == null)
 				return;
 			canvas.appendChild(new_element);
-			// dragging_position = null;
 		}
 		</script>
 	</head>
 	<body onload="javascript: _onload_document();">
-		<h2>dashboard</h2>
-		<div id="div-canvas" class="div-canvas" onmouseup="javascript: _on_dragend_canvas();" onmousedown="javascript: _on_begindrag_canvas();">
-		</div>
-		<div id="div-input-box">
-			いつ <input type="text" name="txt-date" class="txt-date"><br>
-			<br>
-			内容 <input type="text" name="txt-content" class="txt-content">
-			<br>
-			<br>
-			<input type="button" value="OK" onclick="javascript: _onclick_text_apply();">
-			<input type="button" value="CANCEL" onclick="javascript: _onclick_text_cancel();">
-		</div>
+		<form>
+			<!-- ========================================================== -->
+			<!-- Navigation -->
+			<!-- ========================================================== -->
+			<nav class="navbar navbar-inverse navbar-fixed-top">
+				<div class="container">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">ぺたぺたするやつ</a>
+					</div>
+					<div id="navbar" class="navbar-collapse collapse">
+						<ul class="nav navbar-nav">
+							<li class="active"><a href="#">Home</a></li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown"
+										role="button" aria-haspopup="true"
+										aria-expanded="false">Dropdown <span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="#">Action</a></li>
+									<li><a href="#">Another action</a></li>
+									<li><a href="#">Something else here</a></li>
+									<li role="separator" class="divider"></li>
+									<li class="dropdown-header">Nav header</li>
+									<li><a href="#">Separated link</a></li>
+									<li><a href="#">One more separated link</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div><!--/.nav-collapse -->
+				</div>
+			</nav>
+
+			<!-- ========================================================== -->
+			<!-- Main Content -->
+			<!-- ========================================================== -->
+ 			<div class="container theme-showcase" role="main">
+				<div id="div-canvas" class="div-canvas" onmouseup="javascript: _on_dragend_canvas();" onmousedown="javascript: _on_begindrag_canvas();">
+				</div>
+			</div>
+
+			<!-- ========================================================== -->
+			<!-- Modal Sample -->
+			<!-- ========================================================== -->
+			<div id="div-input-box" class="div-input-box">
+				コメント: <input type="text" name="txt-content" class="txt-content">
+				<br>
+				<button type="button" class="btn btn-sm btn-default" onclick="javascript: _on_click_apply();">apply</button>
+				<button type="button" class="btn btn-sm btn-default" onclick="javascript: _onclick_text_cancel();">cancel</button>
+			</div>
+
+			<!-- ========================================================== -->
+			<!-- Modal -->
+			<!-- ========================================================== -->
+			<div id="myModal" class="modal fade" role="dialog">
+				<div class="modal-dialog">
+				<!-- Modal content-->
+				<div class="modal-content">
+				<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal"
+						onclick="javascript: _onclick_text_cancel();">&times;</button>
+				<h4 class="modal-title">Modal Header</h4>
+				</div>
+				<div class="modal-body">
+				<p>Some text in the modal.</p>
+				<div class="form-group">
+				<label for="comment">Comment:</label>
+				<textarea class="form-control" rows="5" id="comment"></textarea>
+				</div>
+				</div>
+				<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal"
+						onclick="javascript: _on_click_apply();">apply</button>
+				</div>
+				</div>
+				</div>
+			</div>
+
+			<!-- ========================================================== -->
+			<!-- footer -->
+			<!-- ========================================================== -->
+
+			<!-- ========================================================== -->
+			<!-- end -->
+			<!-- ========================================================== -->
+
+		</form>
 	</body>
 </html>
